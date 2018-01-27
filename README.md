67030e5d85f35bc972c2af9289e9c3166e3e47d9
enderdrag64 <33301226+enderdrag64@users.noreply.github.com>
2018-01-27 15:54:17 -0500

    Changed for off-season bot + new readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 2018-Robot
 Flight Crew 747's Repository for the 2018 season robot
+
+
+IF TALON PROBLEMS - RECALIBRATE
+	Hold BC CAL
+	Power Cycle Talon
+	Let go of button when light blinks green
+	https://www.ctr-electronics.com/Talon%20SRX%20User's%20Guide.pdf
--- src/org/usfirst/frc/team747/robot/Drive.java
+++ src/org/usfirst/frc/team747/robot/Drive.java
@@ -9,9 +9,9 @@ public class Drive {
 	static Joystick rightDrive = new Joystick(1);
 	static Joystick leftDrive = new Joystick(0);
 	public static TalonSRX leftFrontDrive = new TalonSRX(0);
-	public static TalonSRX leftRearDrive = new TalonSRX(1);
-	public static TalonSRX rightFrontDrive = new TalonSRX(2);
-	public static TalonSRX rightRearDrive = new TalonSRX(3);
+	//public static TalonSRX leftRearDrive = new TalonSRX(2);
+	public static TalonSRX rightFrontDrive = new TalonSRX(3);
+	//public static TalonSRX rightRearDrive = new TalonSRX(5);
 	
 	public static void drive() {
 		double speedModifier;
@@ -33,15 +33,15 @@ public class Drive {
 			speedModifier = 0.0;
 		}
 		
-		CubeSightIntakeSpin.detectCube();
+		//CubeSightIntakeSpin.detectCube();
 
 			
 		double rightJoystickValue = -rightDrive.getRawAxis(1)*speedModifier;
 		double leftJoystickValue = leftDrive.getRawAxis(1)*speedModifier;
 		
 		leftFrontDrive.set(ControlMode.PercentOutput, leftJoystickValue);
-		leftRearDrive.set(ControlMode.PercentOutput, leftJoystickValue);
+		//leftRearDrive.set(ControlMode.PercentOutput, leftJoystickValue);
 		rightFrontDrive.set(ControlMode.PercentOutput, rightJoystickValue);
-		rightRearDrive.set(ControlMode.PercentOutput, rightJoystickValue);
+		//rightRearDrive.set(ControlMode.PercentOutput, rightJoystickValue);
 	}
 }
