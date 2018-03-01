34b1e80d55e610e267143ce062afd7a25e92a682
Brian.Jimenez <brianjimenez869@gmail.com>
2018-02-28 21:54:56 -0500

    GOT THE PID VALUES
    Found the preliminary PID values for linear motion on the robot

--- src/org/usfirst/frc/team217/robot/Robot.java
+++ src/org/usfirst/frc/team217/robot/Robot.java
@@ -20,6 +20,7 @@ package org.usfirst.frc.team217.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import java.util.concurrent.TimeUnit;
 
@@ -30,8 +31,8 @@ import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.can.*;
 
 public class Robot extends IterativeRobot {
-    TalonSRX _rightBackPrimaryTalon = new TalonSRX(1);
-    TalonSRX _rightFrontTalon = new TalonSRX(2);
+    TalonSRX _rightBackPrimaryTalon = new TalonSRX(10);
+    TalonSRX _rightFrontTalon = new TalonSRX(9);
     Joystick _joy = new Joystick(0);
     StringBuilder _sb = new StringBuilder();
 
@@ -42,26 +43,37 @@ public class Robot extends IterativeRobot {
         _rightBackPrimaryTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
         _rightBackPrimaryTalon.setSensorPhase(false);
         _rightBackPrimaryTalon.setInverted(false);
+        
+        _rightFrontTalon.setSensorPhase(false);
+        _rightFrontTalon.setInverted(false);
 
         /* Set relevant frame periods to be at least as fast as periodic rate */
         _rightBackPrimaryTalon.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, Constants.kTimeoutMs);
         _rightBackPrimaryTalon.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, Constants.kTimeoutMs);
+        
+        _rightFrontTalon.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, Constants.kTimeoutMs);
+        _rightFrontTalon.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, Constants.kTimeoutMs);
 
         /* set the peak and nominal outputs */
         _rightBackPrimaryTalon.configNominalOutputForward(0, Constants.kTimeoutMs);
         _rightBackPrimaryTalon.configNominalOutputReverse(0, Constants.kTimeoutMs);
         _rightBackPrimaryTalon.configPeakOutputForward(1, Constants.kTimeoutMs);
         _rightBackPrimaryTalon.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+        
+        _rightFrontTalon.configNominalOutputForward(0, Constants.kTimeoutMs);
+        _rightFrontTalon.configNominalOutputReverse(0, Constants.kTimeoutMs);
+        _rightFrontTalon.configPeakOutputForward(1, Constants.kTimeoutMs);
+        _rightFrontTalon.configPeakOutputReverse(-1, Constants.kTimeoutMs);
 
         /* set closed loop gains in slot0 - see documentation */
         _rightBackPrimaryTalon.selectProfileSlot(Constants.kSlotIdx, Constants.kPIDLoopIdx);
-        _rightBackPrimaryTalon.config_kF(0, 0.2481, Constants.kTimeoutMs);
-        _rightBackPrimaryTalon.config_kP(0, 0, Constants.kTimeoutMs);
+        _rightBackPrimaryTalon.config_kF(0, 0.2031, Constants.kTimeoutMs);
+        _rightBackPrimaryTalon.config_kP(0, 0.4, Constants.kTimeoutMs);
         _rightBackPrimaryTalon.config_kI(0, 0, Constants.kTimeoutMs);
         _rightBackPrimaryTalon.config_kD(0, 0, Constants.kTimeoutMs);
         /* set acceleration and vcruise velocity - see documentation */
-        _rightBackPrimaryTalon.configMotionCruiseVelocity(10000, Constants.kTimeoutMs);
-        _rightBackPrimaryTalon.configMotionAcceleration(10000, Constants.kTimeoutMs);
+        _rightBackPrimaryTalon.configMotionCruiseVelocity(15000, Constants.kTimeoutMs);
+        _rightBackPrimaryTalon.configMotionAcceleration(6000, Constants.kTimeoutMs);
         /* zero the sensor */
         _rightBackPrimaryTalon.setSelectedSensorPosition(0, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
     }
@@ -82,7 +94,7 @@ public class Robot extends IterativeRobot {
 
         if (_joy.getRawButton(1)) {
             /* Motion Magic - 4096 ticks/rev * 10 Rotations in either direction */
-            double targetPos = leftYstick * 4096 * 10.0 * 5.40;
+            double targetPos = leftYstick * 4096 * 2 * 5.4;
             _rightBackPrimaryTalon.set(ControlMode.MotionMagic, targetPos);
 
             /* append more signals to print when in speed mode. */
@@ -103,5 +115,8 @@ public class Robot extends IterativeRobot {
             TimeUnit.MILLISECONDS.sleep(10);
         } catch (Exception e) {
         }
+
+        SmartDashboard.putNumber("Right Encoder Position:", _rightBackPrimaryTalon.getSelectedSensorPosition(0));
+        SmartDashboard.putNumber("Joystick Input:", leftYstick);
     }
 }
